f41674ea78ae074421a3f1223699f46dc063bf76
abhishank05 <abhishekshank05@gmail.com>
2025-07-05 15:49:39 +0530

    cron.js added

--- src/server.js
+++ src/server.js
@@ -4,10 +4,17 @@ import {initDB} from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 
 import transactionsRoute from "./routes/transactionsRoute.js"
+import job from './config/cron';
 
 dotenv.config();
 
 const app=express();
+
+if(process.env.NODE_ENV==="production") job.start();
+
+app.get("/api/health",(req,res)=>{
+    res.status.json({status:"ok"});
+});
 
 //middleware
 app.use(rateLimiter);
